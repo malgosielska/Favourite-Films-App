54e42ff27c7a9104788ff5d9e6943dbc49a63038
malgosielska <malgosielska@hmail.com>
2023-11-19 12:43:56 +0100

    Add switching screens

--- app/src/main/java/com/example/favfilmsapp/MainActivity.kt
+++ app/src/main/java/com/example/favfilmsapp/MainActivity.kt
@@ -1,39 +1,31 @@
 package com.example.favfilmsapp
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.*
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.favfilmsapp.ui.theme.FavFilmsAppTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.foundation.clickable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.favfilmsapp.ui.theme.FavFilmsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +33,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             setContent {
                 FavFilmsAppTheme {
-                    Conversation(SampleData.movies)
+                    val movies = SampleData.movies
+                    Navigation(movies)
                 }
             }
         }
@@ -50,11 +43,14 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MovieCard(movie: Movie) {
-    Row(modifier = Modifier
-        .padding(all = 8.dp))
-        {
-            Image(
+fun MovieCard(movie: Movie, onMovieClick: (Movie) -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .clickable { onMovieClick(movie) }
+    )
+    {
+        Image(
             painter = painterResource(movie.imageResource),
             contentDescription = movie.title,
             modifier = Modifier
@@ -70,46 +66,80 @@ fun MovieCard(movie: Movie) {
             style = MaterialTheme.typography.titleSmall,
             modifier = Modifier.padding(all = 14.dp),
             fontSize = 16.sp
-            )
+        )
     }
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)@Composable
-fun PreviewMovieCard() {
-    FavFilmsAppTheme {
-        Surface {
-            MovieCard(
-                movie = Movie(title="that is my title", imageResource = R.drawable.title, description = "", actors = listOf() , scenes = listOf()),
-            )
-        }
-    }
-}
+//@Preview(name = "Light Mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
+
+//@Composable
+//fun PreviewMovieCard() {
+//    FavFilmsAppTheme {
+//        Surface {
+//            MovieCard(
+//                movie = Movie(title="that is my title", imageResource = R.drawable.title, description = "", actors = listOf() , scenes = listOf()),
+//            )
+//        }
+//    }
+//}
 
 
 @Composable
-fun Conversation(movies: List<Movie>) {
+fun MainScreen(movies: List<Movie>, onMovieClick: (Movie) -> Unit) {
     LazyColumn {
         items(movies) { movie ->
-            MovieCard(movie = movie)
+            MovieCard(movie = movie, onMovieClick)
         }
     }
 }
 
-
-@Preview
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
 @Composable
-fun PreviewConversation() {
-    FavFilmsAppTheme {
-        Conversation(SampleData.movies)
+fun MovieDetails(movie: Movie) {
+
+}
+
+
+//@Preview
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
+//@Composable
+//fun PreviewConversation() {
+//    FavFilmsAppTheme {
+//        MainScreen(SampleData.movies, ())
+//    }
+//}
+
+@Composable
+fun Navigation(movies: List<Movie>) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "mainScreen") {
+        composable("mainScreen") {
+            MainScreen(movies = movies) { selectedMovie ->
+                navController.navigate("movieDetails/${selectedMovie.title}")
+            }
+        }
+
+        composable(
+            route = "movieDetails/{movieTitle}",
+            arguments = listOf(
+                navArgument("movieTitle") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                    nullable = false
+                }
+            )
+        ) { backStackEntry ->
+            val movieTitle = backStackEntry.arguments?.getString("movieTitle") ?: ""
+            val selectedMovie = movies.first { it.title == movieTitle }
+            MovieDetails(selectedMovie)
+        }
     }
 }
